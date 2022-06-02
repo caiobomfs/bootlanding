1ce38897c41aa48300e9182d2d4b919357482436
caiobomfs <caiobomfimfc@gmail.com>
2022-06-02 14:01:17 -0300

    rework no design do slider utilizando o template da documentação do bootstrap

--- js/main.js
+++ js/main.js
@@ -5,22 +5,3 @@ $(document).ready(function(){
 
 
 
-    jQuery(document).ready(function() {
-        
-	jQuery('.carousel[data-type="multi"] .item').each(function(){
-		var next = jQuery(this).next();
-		if (!next.length) {
-			next = jQuery(this).siblings(':first');
-		}
-		next.children(':first-child').clone().appendTo(jQuery(this));
-	  
-		for (var i=0;i<2;i++) {
-			next=next.next();
-			if (!next.length) {
-				next = jQuery(this).siblings(':first');
-			}
-			next.children(':first-child').clone().appendTo($(this));
-		}
-	});
-        
-});
